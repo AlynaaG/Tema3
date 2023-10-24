96a51dd07b51332669fac24a60d96a6ad2a2cdb2
AlynaaG <alynaa.giurca@yahoo.com>
2023-10-24 18:43:23 +0300

    tema3

---
+++ index.js
@@ -0,0 +1,27 @@
+let ziuasaptamanii = 4;
+let numelezilei = "";
+
+switch (ziuasaptamanii) {
+  case 1:
+    numelezilei = "Luni";
+    break;
+  case 2:
+    numelezilei = "Marti";
+    break;
+  case 3:
+    numelezilei = "Miercuri";
+    break;
+  case 4:
+    numelezilei = "Joi";
+    break;
+  case 5:
+    numelezilei = "Vineri";
+    break;
+  case 6:
+    numelezilei = "Sambata";
+    break;
+  case 7:
+    numelezilei = "Duminica";
+    break;
+}
+console.log(numelezilei);
